7c4a96e349c44a62998af2131286b6021173cf35
user <user@users-MacBook-Pro.local>
2017-05-30 07:45:32 +0300

    Some Changes in swift code

---
+++ yeFikir_Debdabe/DetailOne.swift
@@ -0,0 +1,9 @@
+//
+//  DetailOne.swift
+//  yeFikir_Debdabe
+//
+//  Created by user on 5/30/17.
+//  Copyright © 2017 AFC Ethiopia. All rights reserved.
+//
+
+import Foundation
--- yeFikir_Debdabe/TableOne.swift
+++ yeFikir_Debdabe/TableOne.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TableOne: UIViewController {
+class TableOne: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
