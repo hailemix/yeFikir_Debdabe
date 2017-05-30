fec6cef535caba80b71b9e561bffce30ec48c9d0
user <user@users-macbook-pro.local>
2017-05-30 07:15:46 +0300

    New Table Swift files are added

---
+++ yeFikir_Debdabe/TableThree.swift
@@ -0,0 +1,25 @@
+//
+//  TableTwo.swift
+//  yeFikir_Debdabe
+//
+//  Created by user on 5/30/17.
+//  Copyright © 2017 AFC Ethiopia. All rights reserved.
+//
+
+import UIKit
+
+class TableThree: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
