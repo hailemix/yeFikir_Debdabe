44fca65c01fe7c8d6370085cae58f31c818dc5e4
user <user@users-macbook-pro.local>
2017-06-02 10:38:25 +0300

    About_Us is updated

---
+++ About_US.swift
@@ -0,0 +1,19 @@
+//
+//  About_US.swift
+//  yeFikir_Debdabe
+//
+//  Created by user on 6/2/17.
+//  Copyright © 2017 AFC Ethiopia. All rights reserved.
+//
+
+import UIKit
+
+class About_US: UIViewController{
+
+
+    @IBOutlet var About_View: UIView!
+    
+  
+
+}
+
