b49b30baf53cb6e3b7a289525519e2fca14ee1a8
user <user@users-macbook-pro.local>
2017-06-19 05:29:02 +0300

    The journey to favourites is just started!

--- yeFikir_Debdabe/FavoriteTable.swift
+++ yeFikir_Debdabe/FavoriteTable.swift
@@ -13,28 +13,16 @@ class FavouriteTable: UITableViewController{
     
     
     var detailViewController: FavouriteDetail? = nil
-    var objects = ["ገላ ","ሰማይ ቤት","ሰዉ","ይኸዉ!","እንደሚሰማኝ ","የምናፍቅሽ"]
-    
-    let image1 = UIImage(named: "a1")
-    let image2 = UIImage(named: "a3")
-    let image3 = UIImage(named: "a2")
-    let image4 = UIImage(named: "a5")
-    let image5 = UIImage(named: "a58")
-    let image6 = UIImage(named: "a6")
-    let image7 = UIImage(named: "a4")
-    
-    
-    
-    
-    
-    var details = ["በህይወት ልምድ \n\n ዉድ ኑሮ(እዉነትም ዉድ ነህ!) እንዴት ነህ? እንደኮረሪማ ነጋዴ የተከልነዉን ኮረሪማ ማንነታችንን እየቆፈርን ይኸዉ አለን! ኑሮ ንሮ በጣም የምናዝንበት ነገር መብዛቱና መርከሱ ካንተ መናር ጋር በተቃራኒ የቆመ ቢሆንም የልባችንን በኪሳችን ይዘን አለንልህ… አንድ ዳቦ ከ 20 ሳንቲም ተነስቶ 10 ብር መግባቱ  እኛንም ከ ሃያዎቹ እድሜያችን ወደ አስር አመት ዝቅ እንድናደርገዉ ረድቶናል ፤የቤት ሽያጭ ከ 10000 ብር ወደ ሚሊየን ብር መግባቱ እኛንም ከ 10 ሺህ ቀዳዶች ወደ ሚልየን የቀለብ ቀዳዶችን የመድፈን  ውስብስብና ልዩ ጥበብ ባለቤት አርጎናል፡፡አንተ ከፍ እያልክ ፣እየጨመርክ፣እየተወሳሰበክ ስትሄድ…የኛ የሃሳብ ምጥቀት ለምን እንዳልጨመረ አልገባንም! እየዘመንን፣ፋሽን እየተከተልን፣እስታይል በስታይል፣ ፌስ ቡክ በፌስቡክ እየሆንን አለን፡፡አዎን ምክንያት ልንሰጣቸዉ የምንችላቸዉ ሚሊየን ሃሳበች ተደምረዉ አስር ብር  አልሆኑንም! አዎን ልቅምቃሚ ወሬዎቻችን ከጀበና ቡና ስንነሳ  ተበታትነዉ ይቀራሉ! ምን እናርግ!ዉድ ኑሮ እንጀራ ከአንድ ብር ከሃምሳ፣ አምስት ብር ሲገባ የኛ አእምሮ ግን ያዉ እንዳስቀመጥነዉ ነዉ፡፡ የአእምሮአችን መሰላል በትምህርት ልቀት፣በህይወት ልምድ፣ በ ኮንዶሚኒየም ወይም ቀበሌ ቤት ዉስጥ ታጅበን በመኖር ልነለዉጠዉ አልቻልንም፡፡ነገር ግን ያንተን መተኮስንደቲያትር ተመልካች አስተሳሰቦቻችንን ሳንፈትሽ  የምናሽካካ፣የምንተምም፣የምናፏጭ ሰላማዊ ህጻናቶች ነን፡፡አንዱ ሲቀልድ ‹የአዲስ አበባ ህዝብ ቁጥር ስድስት ሚሊየን ሲሆን ህፃናቱን ካልቆጠርናቸዉ ግን 1 ሚሊየን አይሞላም!› ይገርማል ልጅነት ሳይለቀን ቢዝነስ እንጀምራለን፣እሽክርክሮሽ እየተጫወትን እናጋባለን እንግባባለን፣ ጢባጢቤ እያልን ሽምግልናችን ይወረናል…በመጨረሻ የምንመኘዉ ነገር  ልክ እንዳንተ እንደግ፣ከፍ ከፍ አሁንም ከፍ ያርገን  አሜን..(እዚህ ሃገር ሁሉም  ነገር ምኞት ብቻ ነዉ ለምትሉ ሰዎች ወዮላችሁ! አለች የጀበና ቡና!)መልስህን ከስር ባለዉ ፖስታ ሳጥን ቁጥር እንጠብቃለን ",
-                   "John and Mary have been failing in love for more than a year...",
-                   "This is nothing wrong to love a girl and being broken","The first thing that you realise in your single life is he hands of the lord,Jesus coming into you",
-                   "A great romatic love I have ever heared is this:...","The next challenge in life is..."]
+    var objects = [""]
+    var details = [""]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "የወደድኩት"
+      
+       
         // Do any additional setup after loading the view, typically from a nib.
         
         
@@ -45,12 +33,6 @@ class FavouriteTable: UITableViewController{
         }
     }
     
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     
     
@@ -86,54 +68,6 @@ class FavouriteTable: UITableViewController{
         let object = objects[indexPath.row]
         
         cell.textLabel!.text = object
-        
-        
-        
-        if(indexPath.row == 0) {
-            
-            cell.imageView!.image = image1
-            
-            
-        }
-        else if(indexPath.row == 1)
-        {
-            
-            cell.imageView!.image = image3
-            
-            
-        }
-            
-        else if (indexPath.row == 2)
-        {
-            cell.imageView!.image = image5
-            
-            
-            
-        }
-        else if (indexPath.row == 3)
-            
-        {
-            cell.imageView!.image = image6
-            
-        }
-            
-        else if (indexPath.row == 4)
-            
-        {
-            cell.imageView!.image = image7
-            
-        }
-        else if (indexPath.row == 5)
-        {
-            cell.imageView!.image = image1
-            
-        }
-        else if (indexPath.row == 6)
-            
-        {
-            cell.imageView!.image = image3
-            
-        }
         
         return cell
     }
